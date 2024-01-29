41287f340fef8bc587c84bb8ebe42727c2c71fa5
Rajibul Islam <rajibulski110@gmail.com>
2024-01-30 04:37:42 +0530

    created scheduling section

---
+++ nextjs/src/app/mia/sections/scheduling/scheduling.tsx
@@ -0,0 +1,57 @@
+// styles
+import ScheduleButtons from "./components/schedule-buttons";
+import styles from "./scheduling.module.css";
+
+// components
+import HeadingPair from "@/app/components/headingPair";
+
+// image
+import scheduleImage from "../../../../../public/images/schedule.jpg";
+import Image from "next/image";
+
+// scheduling button text and links
+const schedulingData = [
+  {
+    name: "Schedule from Calendly",
+    description:
+      "This is my general availability for the free & paid lessons. Please contact me on Discord before you schedule.",
+    link: "https://calendly.com/miavocalcoach/gen-availability",
+    buttonLabel: "To Calendly",
+  },
+  {
+    name: "Schedule from Discord",
+    description:
+      "Clicking this will open my Discord profile Mia#0006. We can decide a time for our lessons from there.",
+    link: "https://discord.com/users/140513822069882881",
+    buttonLabel: "To my Discord",
+  },
+];
+
+export default function Scheduling() {
+  return (
+    <section
+      className={`block ${styles.section}`}
+      id="scheduling"
+    >
+      <div className={`container ${styles.container}`}>
+        <div className={`${styles.textPortion}`}>
+          <HeadingPair
+            tertiary="Lesson scheduling"
+            heading="Book your lesson"
+            moreMargin={true}
+          />
+          <ScheduleButtons schedulingData={schedulingData} />
+        </div>
+        <div className={styles.imageBox}>
+          <Image
+            className={styles.image}
+            src={scheduleImage}
+            alt="Booking a date illustration art vector graphic"
+            placeholder="blur"
+            blurDataURL="../../../../../public/images/schedule.jpg"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
