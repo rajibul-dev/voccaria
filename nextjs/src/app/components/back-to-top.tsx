bf8c966fa972c65992d51cc1c4d0da976bc15759
Rajibul Islam <rajibulski110@gmail.com>
2024-02-10 22:05:06 +0530

    Added back to top button

---
+++ nextjs/src/app/components/back-to-top.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+// styles
+import styles from "./back-to-top.module.css";
+
+// icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+
+const arrowUpIcon = (
+  <FontAwesomeIcon
+    icon={faArrowUp}
+    className={styles.icon}
+  />
+);
+
+const BackToTop = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollThreshold = 10000;
+      if (window.scrollY >= scrollThreshold) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    showButton && (
+      <button
+        onClick={() => {
+          window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+          });
+        }}
+        className={styles.button}
+      >
+        {arrowUpIcon}
+      </button>
+    )
+  );
+};
+
+export default BackToTop;
--- nextjs/src/app/mia/mia.tsx
+++ nextjs/src/app/mia/mia.tsx
@@ -8,20 +8,24 @@ import Scheduling from "./sections/scheduling/scheduling";
 import Contact from "./sections/contact/contact";
 import Testimonials from "./sections/testimonials/testimonials";
 import Navbar from "./navbar/navbar";
+import BackToTop from "../components/back-to-top";
 
 export default function Mia() {
   return (
-    <main>
-      <Navbar />
-      <About />
-      <Socials />
-      <Patreon />
-      <Objective />
-      <LessonOptions />
-      <Pricing />
-      <Scheduling />
-      <Contact />
-      <Testimonials />
-    </main>
+    <>
+      <main>
+        <Navbar />
+        <About />
+        <Socials />
+        <Patreon />
+        <Objective />
+        <LessonOptions />
+        <Pricing />
+        <Scheduling />
+        <Contact />
+        <Testimonials />
+      </main>
+      <BackToTop />
+    </>
   );
 }
