ce2e14bff60581e6bd2812bb967974fac3b738d9
Rajibul Islam <rajibulski110@gmail.com>
2024-02-14 17:26:30 +0530

    added essay component

---
+++ nextjs/src/app/components/essay.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode } from "react";
+
+// styles
+import styles from "./essay.module.css";
+
+interface EssayProps {
+  children: ReactNode;
+  className?: string;
+}
+interface ParaProps {
+  children: ReactNode;
+  className?: string;
+}
+
+const Essay: React.FC<EssayProps> & {
+  Para: React.FC<ParaProps>;
+} = ({ children, className }) => {
+  return <div className={`${styles.wrapper} ${className}`}>{children}</div>;
+};
+
+const Para: React.FC<ParaProps> = ({ children, className }) => {
+  return (
+    <p className={`goto-paragraph ${styles.para} ${className}`}>{children}</p>
+  );
+};
+
+Essay.Para = Para;
+
+export { Essay, Para };
