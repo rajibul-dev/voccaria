590af936f17ed5b16b4332bce87ac4bd3a23e4b7
Rajibul Islam <rajibulski110@gmail.com>
2024-09-04 15:58:47 +0530

    brought the pricing credentials up into the main root component Pricing.

--- nextjs/src/app/mia/sections/pricing/components/pricing-cards.tsx
+++ nextjs/src/app/mia/sections/pricing/components/pricing-cards.tsx
@@ -3,55 +3,23 @@ import styles from "./pricing-cards.module.css";
 
 // components
 import PricingCardItem from "./pricing-card-item";
+import React from "react";
 
-// create the pricing credentials object
-const pricingDetails = [
-  // Euro dollar instences
-  {
-    currency: "eur",
-    currencySymbol: "€",
-    amount: 40,
-    heading: "Singular 30-minute voice lesson",
-    description: "A short session to get you going in the right direction!",
-  },
-  {
-    currency: "eur",
-    currencySymbol: "€",
-    amount: 80,
-    heading: "Singular 1 hour voice lesson",
-    description:
-      "Enough time for us to give plenty of attention to technique, supervised practice and/or song work!",
-  },
-  {
-    currency: "eur",
-    currencySymbol: "€",
-    amount: 120,
-    heading: "Package of 4 30-minute voice lessons",
-    description: (
-      <>
-        4 Lessons for the price of 3!
-        <br />
-        <s>€160</s> <strong>€120</strong> €30 on average per lesson
-      </>
-    ),
-  },
-  {
-    currency: "eur",
-    currencySymbol: "€",
-    amount: 240,
-    heading: "Package of 4 one hour voice lessons",
-    description: (
-      <>
-        4 Lessons for the price of 3!
-        <br />
-        <s>€320</s> <strong>€240</strong> €60 on average per lesson
-      </>
-    ),
-    recommended: true,
-  },
-];
+// typescript type definition
+interface PricingDetails {
+  currency: string;
+  currencySymbol: string;
+  amount: number;
+  heading: string;
+  description: string | React.ReactNode;
+  recommended?: boolean;
+}
 
-export default function PricingCards() {
+interface PricingCardsProps {
+  pricingDetails: PricingDetails[];
+}
+
+const PricingCards: React.FC<PricingCardsProps> = ({ pricingDetails }) => {
   return (
     <ul className={styles.grid}>
       {pricingDetails.map((item) => (
@@ -67,4 +35,6 @@ export default function PricingCards() {
       ))}
     </ul>
   );
-}
+};
+
+export default PricingCards;
--- nextjs/src/app/mia/sections/pricing/pricing.tsx
+++ nextjs/src/app/mia/sections/pricing/pricing.tsx
@@ -5,6 +5,53 @@ import styles from "./pricing.module.css";
 import HeadingPair from "@/app/components/headingPair";
 import PricingCards from "./components/pricing-cards";
 
+// create the pricing credentials object
+const pricingDetails = [
+  // Euro dollar instences
+  {
+    currency: "eur",
+    currencySymbol: "€",
+    amount: 40,
+    heading: "Singular 30-minute voice lesson",
+    description: "A short session to get you going in the right direction!",
+  },
+  {
+    currency: "eur",
+    currencySymbol: "€",
+    amount: 80,
+    heading: "Singular 1 hour voice lesson",
+    description:
+      "Enough time for us to give plenty of attention to technique, supervised practice and/or song work!",
+  },
+  {
+    currency: "eur",
+    currencySymbol: "€",
+    amount: 120,
+    heading: "Package of 4 30-minute voice lessons",
+    description: (
+      <>
+        4 Lessons for the price of 3!
+        <br />
+        <s>€160</s> <strong>€120</strong> €30 on average per lesson
+      </>
+    ),
+  },
+  {
+    currency: "eur",
+    currencySymbol: "€",
+    amount: 240,
+    heading: "Package of 4 one hour voice lessons",
+    description: (
+      <>
+        4 Lessons for the price of 3!
+        <br />
+        <s>€320</s> <strong>€240</strong> €60 on average per lesson
+      </>
+    ),
+    recommended: true,
+  },
+];
+
 export default function Pricing() {
   return (
     <section
@@ -19,7 +66,7 @@ export default function Pricing() {
           className={styles.heading}
           moreMargin={true}
         />
-        <PricingCards />
+        <PricingCards pricingDetails={pricingDetails} />
       </div>
     </section>
   );
