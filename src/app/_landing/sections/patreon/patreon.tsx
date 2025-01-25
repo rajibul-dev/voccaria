af656b9fcc49cc8cccc54ae4ed4d9f828a3c8187
Rajibul Islam <rajibulski110@gmail.com>
2025-01-25 21:45:58 +0530

    no chakra ui

--- src/app/_landing/sections/patreon/patreon.tsx
+++ src/app/_landing/sections/patreon/patreon.tsx
@@ -9,7 +9,8 @@ import PatreonAvatar from "../../../../../public/images/mia-patron.png";
 // components
 import Button from "@/app/_components/button";
 import HeadingPair from "@/app/_components/headingPair";
-import { ListItem, UnorderedList } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
 
 const patreonFeatures = [
   "I will hear YOUR recordings and react to them live",
@@ -46,19 +47,23 @@ export default function Patreon() {
               </>
             }
             className={styles.heading}
-            tertiaryClassName={`${styles.tertiary} !mb-[10px]`}
+            tertiaryClassName={styles.tertiary}
           />
 
-          <UnorderedList
-            fontWeight={500}
-            spacing={3}
-            lineHeight={1.5}
-            className={styles.ul}
+          <ul
+            role="list"
+            className={`font-medium flex flex-col gap-4 ${styles.ul}`}
           >
             {patreonFeatures.map((item) => (
-              <ListItem key={item}>{item}</ListItem>
+              <li className="flex gap-4" key={item}>
+                <FontAwesomeIcon
+                  icon={faCircle}
+                  className="w-3 translate-y-[5px]"
+                />
+                <span>{item}</span>
+              </li>
             ))}
-          </UnorderedList>
+          </ul>
 
           <Button
             className={styles.btn}
