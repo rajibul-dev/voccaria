6ab7575aabe3772bdd32fe640ed7c60f5cf02f4f
Rajibul Islam <rajibulski110@gmail.com>
2025-01-26 12:23:48 +0530

    old page css stays there

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -88,7 +88,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html className="old-page" lang="en" suppressHydrationWarning>
       <head>
         <script
           id="paypal-script"
