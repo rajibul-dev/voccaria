e6b2e8af05d513f22d4b965b76044e720587247e
Rajibul Islam <rajibulski110@gmail.com>
2025-02-06 16:25:33 +0530

    responsive hero blog index page hero section

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -10,14 +10,14 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <main>
+    <main className="px-6">
       <section className="pt-32">
-        <div className="flex max-w-5xl mx-auto items-center justify-between">
+        <div className="flex max-w-5xl mx-auto items-center justify-between gap-8 max-md:flex-col">
           <div className="">
-            <h1 className="mb-4 text-5xl font-bold text-slate-600 tracking-tight">
+            <h1 className="mb-4 text-5xl font-bold text-slate-600 tracking-tight max-sm:text-4xl max-sm:mb-3">
               Voccaria Blog
             </h1>
-            <p className="text-lg text-slate-800 max-w-[54ch] mb-8">
+            <p className="text-lg text-slate-800 max-w-[54ch] mb-8 max-sm:text-base max-sm:mb-5.5">
               This blog is a resource for teaching you singing / about the
               voice. I'm Mia and I am the founder of Voccaria, and the author of
               this blog. The posts will be structred sequentially like a course.
@@ -31,7 +31,7 @@ export default function Page() {
               src={blogIllustration}
               priority
               alt="Voccaria blog illustration"
-              className="w-70 h-80 object-cover rounded-full hue-rotate-[340deg]"
+              className="w-70 h-80 object-cover rounded-full hue-rotate-[340deg] max-md:hidden"
             />
           </div>
         </div>
