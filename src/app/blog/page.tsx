1a77746e2d31c22c7286b9e3cdb1a0a46ae6acd1
Rajibul Islam <rajibulski110@gmail.com>
2025-02-06 14:48:17 +0530

    blog index page hero section done

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,40 @@
+import { Metadata } from "next";
+import Image from "next/image";
+
+import blogIllustration from "/public/images/blog-illustration.jpg";
+import BlogSearchBar from "../_components/BlogSearchBar";
+
+export const metadata: Metadata = {
+  title: "Blog",
+};
+
 export default function Page() {
-  return <>page</>;
+  return (
+    <main>
+      <section className="pt-32">
+        <div className="flex max-w-5xl mx-auto items-center justify-between">
+          <div className="">
+            <h1 className="mb-4 text-5xl font-bold text-slate-600 tracking-tight">
+              Voccaria Blog
+            </h1>
+            <p className="text-lg text-slate-800 max-w-[54ch] mb-8">
+              This blog is a resource for teaching you singing / about the
+              voice. I'm Mia and I am the founder of Voccaria, and the author of
+              this blog. The posts will be structred sequentially like a course.
+            </p>
+
+            <BlogSearchBar />
+          </div>
+
+          <div>
+            <Image
+              src={blogIllustration}
+              alt="Voccaria blog illustration"
+              className="w-70 h-80 object-cover rounded-full hue-rotate-[340deg]"
+            />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
