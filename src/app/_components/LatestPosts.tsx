2c2c60e00e4304f7f7e92109a46f67c804843c3a
Rajibul Islam <rajibulski110@gmail.com>
2025-02-12 19:18:06 +0530

    from currentSlug to slug

--- src/app/_components/LatestPosts.tsx
+++ src/app/_components/LatestPosts.tsx
@@ -15,17 +15,10 @@ export default async function LatestPosts() {
   return (
     <ul className="flex flex-col items-center gap-5">
       {posts.map(
-        ({
-          title,
-          smallDescription,
-          currentSlug,
-          _id,
-          _createdAt,
-          ofCategory,
-        }) => (
+        ({ title, smallDescription, slug, _id, _createdAt, ofCategory }) => (
           <li key={_id}>
             <Link
-              href={`/blog/${currentSlug}`}
+              href={`/blog/${slug}`}
               className="flex flex-col gap-2 rounded-lg border border-gray-200 bg-gray-100 p-6 shadow-sm hover:bg-gray-200 max-sm:px-3 max-sm:py-5 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
             >
               <h3 className="text-2xl leading-9 font-bold text-slate-500 max-sm:text-xl max-sm:leading-7.5 dark:text-gray-100">
