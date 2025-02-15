d7e9aa7e0606f12774dd21a7a8556c5efdaf5fbf
Rajibul Islam <rajibulski110@gmail.com>
2025-02-16 00:11:32 +0530

    NO FLASH NOW YAY

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -109,8 +109,36 @@ export default async function RootLayout({
           // @ts-ignore
           as="script"
         ></script>
-        <link rel="preload" href="/initialLoadAdjustments.js" as="script" />
-        <script src="/initialLoadAdjustments.js" defer></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function () {
+                const theme = localStorage.getItem("theme") || "light";
+                if (theme === "dark") {
+                  document.documentElement.classList.add("dark");
+                } else {
+                  document.documentElement.classList.remove("dark");
+                }
+              })();
+
+              (function () {
+                function updateHtmlClass() {
+                  const pathname = window.location.pathname;
+                  const isRoot = pathname === "/";
+                  const html = document.documentElement;
+
+                  if (isRoot) {
+                    html.classList.add("old-scaling");
+                  } else {
+                    html.classList.remove("old-scaling");
+                  }
+                }
+
+                updateHtmlClass();
+              })();
+            `,
+          }}
+        />
       </head>
       <body className={`${montserrat.className} antialiased dark:bg-gray-900`}>
         <Providers>
