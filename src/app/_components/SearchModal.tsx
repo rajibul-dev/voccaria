0a3cca1f6871e3d3901a9bd869054933ee61ea4c
Rajibul Islam <rajibulski110@gmail.com>
2025-02-16 20:19:36 +0530

    UI design betterment! category and post types of results has different design with different icon

--- src/app/_components/SearchModal.tsx
+++ src/app/_components/SearchModal.tsx
@@ -7,6 +7,8 @@ import useOutsideClick from "../_hooks/useOutsideClick";
 import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
 import { BlogPost, Category } from "@/models/blogInterfaces";
 import { useRouter } from "next/navigation";
+import { IoDocumentTextOutline } from "react-icons/io5";
+import { LuText } from "react-icons/lu";
 
 export default function SearchModal() {
   const {
@@ -51,17 +53,17 @@ export default function SearchModal() {
             value={query}
             onValueChange={handleSearch}
             placeholder="What are you looking for?"
-            className="mb-5 w-full rounded-lg border border-slate-500 bg-slate-200 px-5 py-2.5 text-lg text-slate-600 outline-none placeholder:text-slate-500/80 dark:bg-gray-800 dark:text-gray-100 dark:placeholder:text-slate-400/80"
+            className="mb-7 w-full rounded-lg border border-slate-500 bg-slate-200 px-5 py-2.5 text-lg text-slate-600 outline-none placeholder:text-slate-500/80 dark:bg-gray-800 dark:text-gray-100 dark:placeholder:text-slate-400/80"
           />
           <span
             onClick={() => toggleSearch(false)}
-            className="absolute top-[50%] right-3 w-fit -translate-y-[90%] cursor-pointer rounded-sm border border-slate-400 bg-slate-200 px-2.5 py-1 text-xs font-bold text-slate-400 transition-colors group-hover:text-slate-500 hover:bg-slate-300 dark:bg-slate-700 group-hover:dark:text-slate-400 dark:hover:bg-slate-800"
+            className="absolute top-[50%] right-3 w-fit -translate-y-[27px] cursor-pointer rounded-sm border border-slate-400 bg-slate-200 px-2.5 py-1 text-xs font-bold text-slate-400 transition-colors group-hover:text-slate-500 hover:bg-slate-300 dark:bg-slate-700 group-hover:dark:text-slate-400 dark:hover:bg-slate-800"
           >
             Esc
           </span>
         </div>
 
-        <Command.CommandList>
+        <Command.CommandList className="[&>div]:flex [&>div]:flex-col [&>div]:gap-1.5">
           {results.length > 0 ? (
             results.map((result: BlogPost & Category, index) => {
               const postUI = (
@@ -73,16 +75,39 @@ export default function SearchModal() {
                   }}
                   className="group cursor-pointer text-slate-700 dark:text-slate-100"
                 >
-                  <div className="rounded-md px-2 py-2 group-hover:bg-slate-200 dark:group-hover:bg-slate-700">
-                    {result.title}
+                  <div className="flex items-baseline gap-3 rounded-md px-2 py-2 group-hover:bg-slate-200 dark:group-hover:bg-slate-700">
+                    <LuText className="shrink-0 translate-y-0.5" />
+                    <span>{result.title}</span>
+                  </div>
+                </Command.CommandItem>
+              );
+
+              const categoryUI = (
+                <Command.CommandItem
+                  key={index}
+                  onSelect={() => {
+                    router.push(`/blog/${result.firstPostSlug}`);
+                    toggleSearch(false);
+                  }}
+                  className="group cursor-pointer text-slate-700 dark:text-slate-100"
+                >
+                  <div className="flex items-center gap-3 rounded-md px-2 py-2 group-hover:bg-slate-200 dark:group-hover:bg-slate-700">
+                    <IoDocumentTextOutline
+                      size={26}
+                      className="-translate-y-0.5"
+                    />
+                    <span className="text-lg font-medium tracking-normal">
+                      {result.title}
+                    </span>
                   </div>
                 </Command.CommandItem>
               );
 
               switch (result.typeOf) {
                 case "post":
-                case "category":
                   return postUI;
+                case "category":
+                  return categoryUI;
                 default:
                   return postUI;
               }
