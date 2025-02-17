a2911b062ad17f5e732fa895d1c9aa2c94839950
Rajibul Islam <rajibulski110@gmail.com>
2025-02-18 01:02:32 +0530

    search modal is now respoinsive for mobile!

--- src/app/_components/SearchModal.tsx
+++ src/app/_components/SearchModal.tsx
@@ -87,7 +87,7 @@ export default function SearchModal() {
     <Command.CommandDialog
       open={isOpen}
       onOpenChange={toggleSearch}
-      className="fixed inset-0 z-100 flex items-center justify-center bg-black/50"
+      className="fixed inset-0 z-100 flex items-center justify-center bg-black/50 max-sm:items-end"
     >
       <DialogTitle className="sr-only">Blog Search</DialogTitle>
       <DialogDescription className="sr-only">
@@ -96,7 +96,7 @@ export default function SearchModal() {
 
       <div
         ref={modalRef}
-        className="max-h-140 w-full max-w-lg overflow-y-auto rounded-lg bg-white p-6 shadow-lg dark:bg-gray-900 [@media(max-height:600px)]:max-h-3/4"
+        className="max-h-140 w-full max-w-lg overflow-y-auto rounded-lg bg-white p-6 shadow-lg max-sm:h-[120%] max-sm:rounded-none dark:bg-gray-900 [@media(max-height:600px)]:max-h-3/4"
       >
         <div className="relative">
           <Command.CommandInput
@@ -104,10 +104,15 @@ export default function SearchModal() {
             onValueChange={handleSearch}
             placeholder="What are you looking for?"
             className="mb-7 w-full rounded-lg border border-slate-500 bg-slate-200 px-5 py-2.5 text-lg text-slate-600 outline-none placeholder:text-slate-500/80 dark:bg-gray-800 dark:text-gray-100 dark:placeholder:text-slate-400/80"
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                e.currentTarget.blur();
+              }
+            }}
           />
           <span
             onClick={() => toggleSearch(false)}
-            className="absolute top-[50%] right-3 w-fit -translate-y-[27px] cursor-pointer rounded-sm border border-slate-400 bg-slate-200 px-2.5 py-1 text-xs font-bold text-slate-400 transition-colors group-hover:text-slate-500 hover:bg-slate-300 dark:bg-slate-700 group-hover:dark:text-slate-400 dark:hover:bg-slate-800"
+            className="absolute top-[50%] right-3 w-fit -translate-y-[27px] cursor-pointer rounded-sm border border-slate-400 bg-slate-200 px-2.5 py-1 text-xs font-bold text-slate-400 transition-colors group-hover:text-slate-500 hover:bg-slate-300 max-sm:hidden dark:bg-slate-700 group-hover:dark:text-slate-400 dark:hover:bg-slate-800"
           >
             Esc
           </span>
