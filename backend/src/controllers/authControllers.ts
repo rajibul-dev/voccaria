0d56499b4e5a81b2a89a2b5a5c8d965f2c206d25
Rajibul Islam <rajibulski110@gmail.com>
2025-04-23 00:46:51 +0530

    register, verifyEmail, newVerificationToken request functions implmented!

--- backend/src/controllers/authControllers.ts
+++ backend/src/controllers/authControllers.ts
@@ -1,20 +1,143 @@
+import crypto from "crypto";
 import { Request, Response } from "express";
+import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
+import { hashPassword } from "../utils/hashPassword.js";
+import { sendAccountVerificationEmail } from "../helpers/sendAccountVerificationEmail.js";
 
-export function register(request: Request, response: Response) {}
+export async function register(request: Request, response: Response) {
+  const { email, password, name } = request.body;
 
-export function verifyEmail(request: Request, response: Response) {}
+  const existingUser = await User.findOne({ email });
 
-export function requestNewVerificationEmail(
+  if (existingUser) {
+    // case: where user exists and is verified
+    if (existingUser.isVerified) {
+      return response.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        message: "An account already exists with this email",
+      });
+    } else {
+      // case: where user exists but is not verified
+      const verificationToken = crypto.randomBytes(40).toString("hex");
+      existingUser.verificationToken = verificationToken;
+      const verificationLink = `${process.env.FRONTEND_URL}/verify-email?token=${verificationToken}&email=${email}`;
+      const isFirstAccount = (await User.countDocuments({})) === 0;
+      const role = isFirstAccount ? "raji" : "user";
+
+      existingUser.role = role;
+      existingUser.name = name;
+      existingUser.hashedPassword = await hashPassword(password);
+      await existingUser.save();
+
+      await sendAccountVerificationEmail({ email, verificationLink, name });
+    }
+  }
+
+  // case: where user does not exist
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "raji" : "user";
+  const verificationToken = crypto.randomBytes(40).toString("hex");
+  const verificationLink = `${process.env.FRONTEND_URL}/verify-email?token=${verificationToken}&email=${email}`;
+  const hashedPassword = await hashPassword(password);
+
+  const user = await User.create({
+    email,
+    hashedPassword,
+    name,
+    role,
+    verificationToken,
+  });
+
+  await sendAccountVerificationEmail({
+    email: user.email,
+    verificationLink,
+    name: user.name,
+  });
+
+  return response.status(StatusCodes.CREATED).json({
+    success: true,
+    message: "Success! Please check your email to verify account.",
+  });
+}
+
+export async function verifyEmail(request: Request, response: Response) {
+  const { verificationToken, email } = request.body;
+
+  const user = await User.findOne({ email, verificationToken });
+
+  if (user.isVerified) {
+    return response.status(StatusCodes.BAD_REQUEST).json({
+      success: false,
+      message: "User is already verified! Please Login.",
+    });
+  }
+
+  if (user.verificationToken !== verificationToken) {
+    return response.status(StatusCodes.BAD_REQUEST).json({
+      success: false,
+      message: "Verification token is invalid or expired",
+    });
+  }
+
+  user.isVerified = true;
+  user.verified = new Date(Date.now());
+  user.verificationToken = null;
+  await user.save();
+
+  return response.status(StatusCodes.OK).json({
+    success: true,
+    message: "Email verified successfully",
+  });
+}
+
+export async function requestNewVerificationEmail(
   request: Request,
   response: Response
-) {}
+) {
+  const { email } = request.body;
 
-export function forgotPassword(request: Request, response: Response) {}
+  const user = await User.findOne({ email });
 
-export function login(request: Request, response: Response) {}
+  if (!user) {
+    return response.status(StatusCodes.BAD_REQUEST).json({
+      success: false,
+      message: "User not found",
+    });
+  }
 
-export function resetPassword(request: Request, response: Response) {}
+  if (user.isVerified) {
+    return response.status(StatusCodes.BAD_REQUEST).json({
+      success: false,
+      message: "User is already verified! Please Login.",
+    });
+  }
 
-export function changePassword(request: Request, response: Response) {}
+  const verificationToken = crypto.randomBytes(40).toString("hex");
+  const verificationLink = `${process.env.FRONTEND_URL}/verify-email?token=${verificationToken}&email=${email}`;
 
-export function logout(request: Request, response: Response) {}
+  user.verificationToken = verificationToken;
+  await user.save();
+
+  await sendAccountVerificationEmail({
+    email: user.email,
+    verificationLink,
+    name: user.name,
+  });
+
+  return response.status(StatusCodes.OK).json({
+    success: true,
+    message:
+      "New verification email sent! Please check your email to verify account.",
+  });
+}
+
+export async function forgotPassword(request: Request, response: Response) {}
+
+export async function login(request: Request, response: Response) {}
+
+export async function resetPassword(request: Request, response: Response) {}
+
+export async function changePassword(request: Request, response: Response) {}
+
+export async function logout(request: Request, response: Response) {}
