0eb80e59729e57234c42fa99a5091d47764916d2
Rajibul Islam <rajibulski110@gmail.com>
2025-05-02 19:16:57 +0530

    feat(pricing): update pricing on countdown end in June 1

--- apps/web-frontend/src/app/_landing/sections/pricing/pricing.tsx
+++ apps/web-frontend/src/app/_landing/sections/pricing/pricing.tsx
@@ -60,11 +60,59 @@ const pricingDetails = [
   },
 ];
 
+const newPricingDetails = [
+  // Euro dollar instences
+  {
+    currency: "eur",
+    currencySymbol: "€",
+    amount: 50,
+    heading:
+      "Written Recording Review + Custom Vocal Development Plan (Offline Written/Video Feedback, One-Time)",
+    description:
+      "A personalized, in-depth review of your vocal habits and techniques, along with detailed feedback on what can be improved and how. You'll also receive a custom roadmap designed to help you achieve your vocal goals with clear, actionable steps.",
+    className: "recordingReviewCard",
+    moreDescription:
+      "Send your recordings and a detailed description of the sound and style you're aiming for to begin your customized vocal journey!",
+  },
+  {
+    currency: "eur",
+    currencySymbol: "€",
+    amount: 50,
+    heading: "Singular 30-minute voice lesson",
+    description: "A short session to get you going in the right direction!",
+  },
+  {
+    currency: "eur",
+    currencySymbol: "€",
+    amount: 90,
+    heading: "Singular 60-minute voice lesson",
+    description:
+      "Enough time for us to give plenty of attention to technique, supervised practice and/or song work!",
+  },
+  {
+    currency: "eur",
+    currencySymbol: "€",
+    amount: 160,
+    heading: "4 × 30-minute voice lessons",
+    description: "€40 per lesson!",
+    recommendedRow: true,
+  },
+  {
+    currency: "eur",
+    currencySymbol: "€",
+    amount: 320,
+    heading: "4 × 60-minute voice lessons",
+    description: "€80 per lesson!",
+    recommended: true,
+    recommendedRow: true,
+  },
+];
+
 // testing countdown
-// const targetDateOutside: any = addSeconds(new Date(), 5);
+const targetDateOutside: any = addSeconds(new Date(), 5);
 
 // June 1st 12 AM EEST
-const targetDateOutside: any = new Date("2025-06-01T00:00:00Z");
+// const targetDateOutside: any = new Date("2025-06-01T00:00:00Z");
 
 export default function Pricing() {
   const { timeLeft, isComplete } = useCountdown(targetDateOutside);
@@ -87,15 +135,19 @@ export default function Pricing() {
         />
 
         {/* Countdown UI */}
-        <div className="mx-auto mt-[-2rem] mb-[6rem] max-w-220 rounded-lg bg-orange-100 px-4 py-3 shadow-md">
-          <p className="text-center text-[1.8rem] leading-11">
-            There will be pricing changes from June 1
-            <br />
-            <strong>Countdown: {countdownString}</strong>
-          </p>
-        </div>
+        {!isComplete && (
+          <div className="mx-auto mt-[-2rem] mb-[6rem] max-w-220 rounded-lg bg-orange-100 px-4 py-3 shadow-md">
+            <p className="text-center text-[1.8rem] leading-11">
+              There will be pricing changes from June 1
+              <br />
+              <strong>Countdown: {countdownString}</strong>
+            </p>
+          </div>
+        )}
 
-        <PricingCards pricingDetails={pricingDetails} />
+        <PricingCards
+          pricingDetails={isComplete ? newPricingDetails : pricingDetails}
+        />
       </div>
     </section>
   );
