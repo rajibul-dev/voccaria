c7884b25126e8b45eeaa6feab2c5fd7b3e767e90
Rajibul Islam <rajibulski110@gmail.com>
2025-05-04 01:07:57 +0530

    fix(footer): increase gap between logo and c-text in footer component

--- apps/web-frontend/src/app/_components/Footer.tsx
+++ apps/web-frontend/src/app/_components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-300 py-8 dark:bg-gray-700">
       <div className="grid grid-cols-3 items-center justify-center px-3">
-        <div className="flex flex-col gap-3 border-b border-b-gray-700 pb-2">
+        <div className="flex flex-col gap-5 border-b border-b-gray-700 pb-2">
           <TwoLogosAndSlash />
           <p className="text-end text-gray-700">
             © {year} Voccaria. All rights reserved.
