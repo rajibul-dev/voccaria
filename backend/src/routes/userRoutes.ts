1ba7bea21548753c0ab44a81979a5f1759e19500
Rajibul Islam <rajibulski110@gmail.com>
2025-05-08 19:29:56 +0530

    fix(routes): correct order was important

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -5,23 +5,23 @@ import {
   removeAvatar,
   showMe,
   updateAvatar,
-  updateUser,
+  updateMe,
 } from "../controllers/userControllers.js";
 import { authorizeUser } from "../middlewares/authorizeUserMiddleware.js";
-import { requireRole } from "../middlewares/requireRole.js";
 
 const router = Router();
 
 export const allStaffRoles = ["staff", "raji", "mia"];
 
 router.route("/").get(authorizeUser, getAllUsers);
-router.route("/:id").get(authorizeUser, getUser);
 
-router.route("/me").get(authorizeUser, showMe).patch(authorizeUser, updateUser);
+router.route("/me").get(authorizeUser, showMe).patch(authorizeUser, updateMe);
 
 router
   .route("/me/avatar")
   .patch(authorizeUser, updateAvatar)
   .delete(authorizeUser, removeAvatar);
+
+router.route("/:id").get(authorizeUser, getUser);
 
 export default router;
