3c83f43fbee60aed5793b4944e84eaffea396b2b
Rajibul Islam <rajibulski110@gmail.com>
2025-05-08 18:21:23 +0530

    fix(routes): remove role requirement for getAllUsers and getUser routes

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -14,13 +14,11 @@ const router = Router();
 
 const allStaffRoles = ["staff", "raji", "mia"];
 
-router
-  .route("/")
-  .get(authorizeUser, requireRole(...allStaffRoles), getAllUsers);
+router.route("/").get(authorizeUser, getAllUsers);
 router.route("/showMe").get(authorizeUser, showMe);
 // router.route("/updateUser").patch(authenticateUser, updateUser);
 // router.route("/updateAvatar").patch(authenticateUser, updateAvatar);
 // router.route("/removeAvatar").delete(authenticateUser, removeAvatar);
-router.route("/:id").get(authorizeUser, requireRole(...allStaffRoles), getUser);
+router.route("/:id").get(authorizeUser, getUser);
 
 export default router;
