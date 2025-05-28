3cf4f6377e2d6f228d562712f8d70d67ced5f208
Rajibul Islam <rajibulski110@gmail.com>
2025-05-28 15:59:39 +0530

    feat(LoginForm): complete login form UI with forgot password and account creation links

--- apps/web-frontend/src/app/_components/LoginFormEmailPassword.tsx
+++ apps/web-frontend/src/app/_components/LoginFormEmailPassword.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import FancyInput from "./FancyInput";
+import Link from "next/link";
 
 export default function LoginFormEmailPassword() {
   const [email, setEmail] = useState("");
@@ -21,6 +22,25 @@ export default function LoginFormEmailPassword() {
       >
         Password
       </FancyInput>
+
+      <button className="manual-auth-btn attractive-text-shadow">Login</button>
+
+      <Link
+        className="text-center text-sm text-gray-500 underline-offset-4 hover:underline"
+        href={"/auth/forgot-password"}
+      >
+        Forgot password?
+      </Link>
+
+      <div className="mt-6">
+        <span>Don't have an account?</span>{" "}
+        <Link
+          className="text-my-pink-600 font-medium underline-offset-4 hover:underline"
+          href={"/auth/register"}
+        >
+          Create an account
+        </Link>
+      </div>
     </form>
   );
 }
