5bf1f0b5fb2a21dcdd9ffee0f7ca138da7d253af
Rajibul Islam <rajibulski110@gmail.com>
2025-05-29 15:45:53 +0530

    fix(FancyInput): made it faster basically! Added pointer-events-none and select-none classes to label for better accessibility and faster feel.

--- apps/web-frontend/src/app/_components/FancyInput.tsx
+++ apps/web-frontend/src/app/_components/FancyInput.tsx
@@ -35,7 +35,7 @@ export default function FancyInput({
       <label
         htmlFor={name}
         className={clsx(
-          "peer-focus:text-my-pink-600 peer-focus:dark:text-my-pink-500 absolute start-5 top-2 z-10 origin-[0] -translate-y-5 scale-75 transform cursor-text bg-gray-100 px-2 text-base text-gray-700 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:mt-[0.5px] peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-5 peer-focus:scale-75 peer-focus:px-2 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:bg-gray-900 dark:text-gray-400",
+          "peer-focus:text-my-pink-600 peer-focus:dark:text-my-pink-500 pointer-events-none absolute start-5 top-2 z-10 origin-[0] -translate-y-5 scale-75 transform cursor-text bg-gray-100 px-2 text-base text-gray-700 duration-300 select-none peer-placeholder-shown:top-1/2 peer-placeholder-shown:mt-[0.5px] peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-5 peer-focus:scale-75 peer-focus:px-2 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:bg-gray-900 dark:text-gray-400",
           labelClassName,
         )}
       >
