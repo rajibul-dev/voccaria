109710dee2ece466a5bd1267953d78a0fb66bc84
Rajibul Islam <rajibulski110@gmail.com>
2025-05-29 15:53:21 +0530

    fix(AuthLayout): add padding to the main container for improved layout spacing

--- apps/web-frontend/src/app/auth/layout.tsx
+++ apps/web-frontend/src/app/auth/layout.tsx
@@ -8,7 +8,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50 dark:bg-gray-900">
+    <div className="flex min-h-screen items-center justify-center bg-gray-50 pt-10 pb-14 dark:bg-gray-900">
       <main className="flex flex-col items-center justify-center">
         <Logo className="mb-7 w-60" />
         {children}
