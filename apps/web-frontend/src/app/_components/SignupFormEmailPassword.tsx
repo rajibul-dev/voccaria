a2e1d5a437798ae069190454e36f202faf3d73ea
Rajibul Islam <rajibulski110@gmail.com>
2025-05-29 15:54:38 +0530

    feat(SignupFormEmailPassword): implement signup form with email and password fields

---
+++ apps/web-frontend/src/app/_components/SignupFormEmailPassword.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useState } from "react";
+import FancyInput from "./FancyInput";
+import Link from "next/link";
+
+export default function SignupFormEmailPassword() {
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  return (
+    <form className="flex flex-col gap-4">
+      <FancyInput
+        type="text"
+        name="name"
+        value={displayName}
+        onChange={setDisplayName}
+      >
+        Display Name
+      </FancyInput>
+
+      <FancyInput type="email" name="email" value={email} onChange={setEmail}>
+        Email
+      </FancyInput>
+
+      <FancyInput
+        type="password"
+        name="password"
+        value={password}
+        onChange={setPassword}
+      >
+        Password
+      </FancyInput>
+
+      <FancyInput
+        type="password"
+        name="confirm-password"
+        value={confirmPassword}
+        onChange={setConfirmPassword}
+      >
+        Re-enter Password
+      </FancyInput>
+
+      <button className="manual-auth-btn attractive-text-shadow">
+        Create Account
+      </button>
+
+      <div className="mt-6 text-center">
+        <span>Already have an account?</span>{" "}
+        <Link
+          className="text-my-pink-600 font-medium underline-offset-4 hover:underline"
+          href={"/auth/login"}
+        >
+          Login
+        </Link>
+      </div>
+    </form>
+  );
+}
--- apps/web-frontend/src/app/auth/register/page.tsx
+++ apps/web-frontend/src/app/auth/register/page.tsx
@@ -1,9 +1,26 @@
+import GoogleOAuthButton from "@/app/_components/GoogleOAuthButton";
+import LoginFormEmailPassword from "@/app/_components/LoginFormEmailPassword";
+import OrDivider from "@/app/_components/OrDivider";
+import SignupFormEmailPassword from "@/app/_components/SignupFormEmailPassword";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Create Account",
+  title: "Register",
+  description: "Create a new account to access the Voccaria platform.",
+  keywords: ["signup", "register", "create account", "new user"],
 };
 
 export default function Page() {
-  return <>register page</>;
+  return (
+    <div className="w-full">
+      <h1 className="mb-8 text-center text-3xl font-bold text-slate-800 dark:text-slate-200">
+        Create your account
+      </h1>
+      <section>
+        <GoogleOAuthButton />
+        <OrDivider />
+        <SignupFormEmailPassword />
+      </section>
+    </div>
+  );
 }
