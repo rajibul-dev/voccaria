4939e2a28e54a0185181c4c0851a6e0b0eecbcbd
Rajibul Islam <rajibulski110@gmail.com>
2025-05-31 19:00:30 +0530

    chore(OldFooter): a boolean toggle to control footer dark or light mode in the old landing page

--- apps/web-frontend/src/app/_components/old-two-logo-and-slash.tsx
+++ apps/web-frontend/src/app/_components/old-two-logo-and-slash.tsx
@@ -1,6 +1,8 @@
 import miaHeartLogo from "/public/images/mia-heart-logo.png";
 import voccariaLogo from "/public/images/logo-voccaria.png";
 import Image from "next/image";
+import clsx from "clsx";
+import { wantDarkFooter } from "../_old-components/old-footer";
 
 export default function OldTwoLogosAndSlash() {
   return (
@@ -12,12 +14,20 @@ export default function OldTwoLogosAndSlash() {
       />
 
       {/* Slash */}
-      <div className="h-[8rem] w-[1px] shrink-0 rotate-25 bg-gray-700 dark:bg-gray-300"></div>
+      <div
+        className={clsx(
+          "h-[8rem] w-[1px] shrink-0 rotate-25 bg-gray-700",
+          wantDarkFooter && "dark:bg-gray-300",
+        )}
+      ></div>
 
       <Image
         src={voccariaLogo}
         alt="Voccaria logo"
-        className="ml-[2.4rem] w-[22rem] max-[1100px]:w-[17.6rem] max-lg:w-[23rem] dark:brightness-105"
+        className={clsx(
+          "ml-[2.4rem] w-[22rem] max-[1100px]:w-[17.6rem] max-lg:w-[23rem]",
+          wantDarkFooter && "dark:brightness-105",
+        )}
       />
     </div>
   );
--- apps/web-frontend/src/app/_old-components/old-footer.tsx
+++ apps/web-frontend/src/app/_old-components/old-footer.tsx
@@ -6,10 +6,17 @@ import Link from "next/link";
 import { footerLinks } from "../_components/Footer";
 import OldTwoLogosAndSlash from "../_components/old-two-logo-and-slash";
 
+export const wantDarkFooter = false; // for old landing page
+
 export default function OldFooter() {
   const year = new Date().getFullYear();
   return (
-    <footer className="bg-gray-300 pt-[5.6rem] pb-[5.6rem] max-lg:py-[8rem] dark:bg-gray-700">
+    <footer
+      className={clsx(
+        "bg-gray-300 pt-[5.6rem] pb-[5.6rem] max-lg:py-[8rem]",
+        wantDarkFooter && "dark:bg-gray-700",
+      )}
+    >
       <div
         className={clsx(
           "mx-auto grid grid-cols-3 items-end justify-center gap-[4.8rem] px-[1.2rem] max-[1100px]:grid-cols-1 max-[1100px]:!gap-[3.2rem] max-[1100px]:px-[2rem] max-xl:gap-[1.6rem] max-lg:!gap-[5.4rem]",
@@ -17,13 +24,15 @@ export default function OldFooter() {
       >
         <div
           className={clsx(
-            "mb-[.6rem] flex flex-col gap-[2rem] justify-self-end border-b border-b-gray-800 pb-[.8rem] max-[1100px]:order-2 max-[1100px]:max-w-[72ch] max-[1100px]:justify-self-center dark:border-b-gray-300",
+            "mb-[.6rem] flex flex-col gap-[2rem] justify-self-end border-b border-b-gray-800 pb-[.8rem] max-[1100px]:order-2 max-[1100px]:max-w-[72ch] max-[1100px]:justify-self-center",
+            wantDarkFooter && "dark:border-b-gray-300",
           )}
         >
           <OldTwoLogosAndSlash />
           <p
             className={clsx(
-              "text-end text-[1.6rem] leading-[1.5] text-gray-800 max-[1100px]:text-center max-md:text-[2rem] max-md:leading-[1.7] dark:text-gray-300",
+              "text-end text-[1.6rem] leading-[1.5] text-gray-800 max-[1100px]:text-center max-md:text-[2rem] max-md:leading-[1.7]",
+              wantDarkFooter && "dark:text-gray-300",
             )}
           >
             © {year} Voccaria. All rights reserved.
@@ -43,7 +52,10 @@ export default function OldFooter() {
             >
               <h2
                 id={`${section.section}-heading`}
-                className="mb-[1.1rem] text-[1.6rem] leading-[1.5] font-semibold text-gray-800 max-md:text-[2rem] max-md:leading-[1.7] dark:text-white"
+                className={clsx(
+                  "mb-[1.1rem] text-[1.6rem] leading-[1.5] font-semibold text-gray-800 max-md:text-[2rem] max-md:leading-[1.7]",
+                  wantDarkFooter && "dark:text-white",
+                )}
               >
                 {section.section}
               </h2>
@@ -52,7 +64,11 @@ export default function OldFooter() {
                   <li key={link.name}>
                     <Link
                       href={link.href}
-                      className="text-[1.4rem] leading-[1.5] text-gray-700 transition-colors hover:text-gray-900 max-md:text-[1.8rem] max-md:leading-[1.7] dark:text-gray-300 dark:hover:text-white"
+                      className={clsx(
+                        "text-[1.4rem] leading-[1.5] text-gray-700 transition-colors hover:text-gray-900 max-md:text-[1.8rem] max-md:leading-[1.7]",
+                        wantDarkFooter &&
+                          "dark:text-gray-300 dark:hover:text-white",
+                      )}
                       target="_blank"
                       rel="noopener noreferrer"
                     >
@@ -68,17 +84,23 @@ export default function OldFooter() {
         {/* Showing my portfolio */}
         <div
           className={clsx(
-            "mb-[.6rem] max-w-[44ch] justify-self-start border-b border-b-gray-800 pb-[.8rem] max-[1100px]:order-3 max-[1100px]:justify-self-center max-lg:max-w-[57ch] max-sm:w-full dark:border-b-gray-300",
+            "mb-[.6rem] max-w-[44ch] justify-self-start border-b border-b-gray-800 pb-[.8rem] max-[1100px]:order-3 max-[1100px]:justify-self-center max-lg:max-w-[57ch] max-sm:w-full",
+            wantDarkFooter && "dark:border-b-gray-300",
           )}
         >
           <p
             className={clsx(
-              "text-end text-[1.8rem] text-gray-800 max-[1100px]:text-start max-md:text-[2.2rem] max-md:leading-[1.6] dark:text-gray-300",
+              "text-end text-[1.8rem] text-gray-800 max-[1100px]:text-start max-md:text-[2.2rem] max-md:leading-[1.6]",
+              wantDarkFooter && "dark:text-gray-300",
             )}
           >
             This website is designed and built by{" "}
             <a
-              className="text-my-pink-600 hover:text-my-pink-500 dark:text-my-pink-300 dark:hover:text-my-pink-400 font-medium transition-colors"
+              className={clsx(
+                "text-my-pink-600 hover:text-my-pink-500 font-medium transition-colors",
+                wantDarkFooter &&
+                  "dark:text-my-pink-300 dark:hover:text-my-pink-400",
+              )}
               target="_blank"
               rel="noopener noreferrer"
               href="https://www.notion.so/Portfolio-1cc7bdc1208c80bfa968c49fbf67118f?pvs=4"
