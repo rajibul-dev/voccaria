9aeac4e1438376328ff7a35c30ef018254b6ec23
Rajibul Islam <rajibulski110@gmail.com>
2025-05-31 18:50:03 +0530

    fix(BlogHeroButton): update button colors for improved consistancy in design

--- apps/web-frontend/src/app/_components/BlogHeroButton.tsx
+++ apps/web-frontend/src/app/_components/BlogHeroButton.tsx
@@ -31,9 +31,11 @@ export default function BlogHeroButton({
   return (
     <button
       onClick={handleContinue}
-      className="dark:bg-my-pink-600 dark:hover:bg-my-pink-500 hover:bg-dull-violet-500 bg-dull-violet-600 cursor-pointer rounded-lg px-5 text-lg font-medium whitespace-nowrap text-white transition-colors max-sm:max-w-50 max-sm:grow-1 max-sm:px-4 max-sm:text-base"
+      className="dark:bg-my-pink-600 dark:hover:bg-my-pink-500 hover:bg-old-btn-pink-hover bg-old-btn-pink cursor-pointer rounded-lg px-5 text-lg font-medium whitespace-nowrap text-white transition-colors max-sm:max-w-50 max-sm:grow-1 max-sm:px-4 max-sm:text-base"
     >
       {lastRead ? "Continue reading" : "Start reading"}
     </button>
   );
 }
+
+// beofre button color was bg-dull-violet-600 hover:bg-dull-violet-500
