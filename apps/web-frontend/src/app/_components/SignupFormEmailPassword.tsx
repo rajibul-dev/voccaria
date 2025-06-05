6e2f55e8d5bb6cb6a2c6e310d80e41f0cb012359
Rajibul Islam <rajibulski110@gmail.com>
2025-06-05 20:59:31 +0530

    feat-checkpoint(SignupFormEmailPassword): implement signup functionality with form validation and error handling

--- apps/web-frontend/src/app/_components/SignupFormEmailPassword.tsx
+++ apps/web-frontend/src/app/_components/SignupFormEmailPassword.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import FancyInput from "./FancyInput";
 import Link from "next/link";
+import { expressBackendBaseRESTOrigin } from "@/_constants/backendOrigins";
 
 export default function SignupFormEmailPassword() {
   const [displayName, setDisplayName] = useState("");
@@ -10,18 +11,62 @@ export default function SignupFormEmailPassword() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
+  async function handleSignup() {
+    if (password !== confirmPassword) {
+      console.error("Passwords do not match");
+      return;
+    }
+
+    try {
+      const res = await fetch(`${expressBackendBaseRESTOrigin}/auth/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: displayName,
+          email,
+          password,
+        }),
+      });
+
+      const data = await res.json();
+      console.log(data);
+
+      if (!res.ok) {
+        console.error("Signup failed:", data.message);
+        return;
+      }
+    } catch (error) {
+      console.error("Something went wrong:", error);
+    }
+  }
+
   return (
-    <form className="flex flex-col gap-4">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSignup();
+      }}
+      className="flex flex-col gap-4"
+    >
       <FancyInput
         type="text"
         name="name"
         value={displayName}
         onChange={setDisplayName}
+        required
       >
         Display Name
       </FancyInput>
 
-      <FancyInput type="email" name="email" value={email} onChange={setEmail}>
+      <FancyInput
+        type="email"
+        name="email"
+        value={email}
+        onChange={setEmail}
+        required
+      >
         Email
       </FancyInput>
 
@@ -30,6 +75,7 @@ export default function SignupFormEmailPassword() {
         name="password"
         value={password}
         onChange={setPassword}
+        required
       >
         Password
       </FancyInput>
@@ -39,6 +85,7 @@ export default function SignupFormEmailPassword() {
         name="confirm-password"
         value={confirmPassword}
         onChange={setConfirmPassword}
+        required
       >
         Re-enter Password
       </FancyInput>
