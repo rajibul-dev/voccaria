cc972f3bec4ae9d3de0b3abb9e710640866199f5
Rajibul Islam <rajibulski110@gmail.com>
2025-06-20 01:13:00 +0530

    feat(forgot-password): add forgot password page submitting which sends password reset link

---
+++ apps/web-frontend/src/app/auth/forgot-password/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { expressBackendBaseRESTOrigin } from "@/_constants/backendOrigins";
+import FancyInput from "@/app/_components/FancyInput";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+export default function page() {
+  const [email, setEmail] = useState("");
+
+  async function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    const res = await fetch(
+      `${expressBackendBaseRESTOrigin}/auth/forgot-password`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      },
+    );
+
+    const jsonResponse = await res.json();
+    if (res.ok) {
+      console.log("Password reset link sent:", jsonResponse);
+      toast.success("Password reset link sent! Please check your email.");
+      setEmail("");
+    } else {
+      console.error("Error sending password reset link:", jsonResponse);
+    }
+  }
+
+  return (
+    <div className="w-full">
+      <h1 className="mb-4 text-center text-3xl font-bold text-slate-800 max-sm:text-2xl dark:text-slate-200">
+        Forgot Password
+      </h1>
+      <section className="w-full">
+        <p className="mb-6 text-center text-gray-700 dark:text-gray-300">
+          Please enter your email address to receive a password reset link.
+        </p>
+        <form className="mx-auto max-w-md" onSubmit={handleSubmit}>
+          <FancyInput value={email} onChange={setEmail} inputClassName="mb-6">
+            Email
+          </FancyInput>
+          <button
+            type="submit"
+            className="manual-auth-btn attractive-text-shadow w-full"
+          >
+            Send Reset Link
+          </button>
+        </form>
+      </section>
+    </div>
+  );
+}
