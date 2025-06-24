134afdbc743620d00ab2a99e3d7f756fc829b9f2
Rajibul Islam <rajibulski110@gmail.com>
2025-06-24 18:10:00 +0530

    fix(cors): update allowed origins for CORS policy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -28,8 +28,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:3000",
   "https://voccaria.com",
-  "https://voccaria-git-dev-deploy-rajidevteams-projects.vercel.app",
-  "https://voccaria-dev.vercel.app",
+  "https://dev.voccaria.com",
   undefined, // Allow requests with no origin (like mobile apps or curl requests)
 ];
 
