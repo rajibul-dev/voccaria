f96f4b2a1ee246d66e659e78f23993e766001846
Rajibul Islam <rajibulski110@gmail.com>
2025-07-08 17:26:54 +0530

    feat(profile): implement user profile display with avatar and membership date

--- apps/web-frontend/src/app/app/profile/page.tsx
+++ apps/web-frontend/src/app/app/profile/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+import { useAuth } from "@/app/_context/AuthContext";
+import { Avatar } from "@mui/material";
+import { formatDate } from "date-fns";
+
 export default function page() {
-  return <>Profile</>;
+  const { user } = useAuth();
+
+  return (
+    <div>
+      <div className="flex items-center gap-8">
+        <Avatar src={user?.avatar} sx={{ width: 170, height: 170 }} />
+        <div className="flex flex-col gap-1.5">
+          <h1 className="text-4xl font-extrabold text-gray-800 dark:text-gray-100">
+            {user?.name}
+          </h1>
+          <p className="text-lg font-extrabold text-slate-500 uppercase dark:text-slate-400">
+            Member since {formatDate(user?.createdAt || "", "MMMM dd, yyyy")}
+          </p>
+        </div>
+      </div>
+
+      {/* bio and edit profile button */}
+    </div>
+  );
 }
