bcc56d3128a23e9e4e180610e883ea0d0e4737de
Rajibul Islam <rajibulski110@gmail.com>
2025-07-11 22:44:38 +0530

    fix(profile): page can't be 'use client' so I had to move profile into its client component

---
+++ apps/web-frontend/src/app/_components/MyProfile.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useState } from "react";
+import { useAuth } from "../_context/AuthContext";
+import { Avatar } from "@mui/material";
+import { formatDate } from "date-fns";
+import Button from "./Button";
+import EditProfileForm from "./EditProfileForm";
+
+export default function MyProfile() {
+  const [isEditing, setIsEditing] = useState(false);
+  const { user, setUser } = useAuth();
+
+  return (
+    <>
+      {!isEditing && (
+        <div className="flex items-center gap-8">
+          <Avatar src={user?.avatar} sx={{ width: 170, height: 170 }} />
+          <div className="flex flex-col gap-1.5">
+            <h1 className="text-4xl font-extrabold text-gray-800 dark:text-gray-100">
+              {user?.name}
+            </h1>
+            <p className="text-lg font-extrabold text-slate-500 uppercase dark:text-slate-400">
+              Member since {formatDate(user?.createdAt || "", "MMMM dd, yyyy")}
+            </p>
+          </div>
+        </div>
+      )}
+
+      {!isEditing && user?.bio && (
+        <p className="text-base text-gray-800 dark:text-gray-200">{user.bio}</p>
+      )}
+
+      {!isEditing && (
+        <Button onClick={() => setIsEditing(true)} className="px-10">
+          Edit profile
+        </Button>
+      )}
+
+      {isEditing && (
+        <EditProfileForm
+          user={user}
+          setIsEditing={setIsEditing}
+          setUser={setUser}
+        />
+      )}
+    </>
+  );
+}
