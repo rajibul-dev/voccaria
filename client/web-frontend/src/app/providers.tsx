5838c8df7eaf29f7c64a84a05faa4ff744636301
Rajibul Islam <rajibulski110@gmail.com>
2026-01-03 02:25:25 +0530

    fixed odd toast behaviour

--- client/web-frontend/src/app/providers.tsx
+++ client/web-frontend/src/app/providers.tsx
@@ -64,42 +64,40 @@ export function Providers({
     <AppRouterCacheProvider>
       <ThemeProvider theme={muiTheme}>
         <QueryClientProvider client={queryClient}>
+          <Toaster
+            position="bottom-center"
+            gutter={12}
+            containerStyle={{
+              margin: "8px",
+            }}
+            toastOptions={{
+              className: clsx(
+                "!bg-gray-100 !text-gray-800 !font-medium leading-[1.3] !border !border-gray-300 [&>div]:!my-0",
+                {
+                  "dark:!bg-gray-700 dark:!text-gray-200 !text-base dark:!border-gray-600":
+                    !isRoot,
+                  "": isRoot,
+                },
+              ),
+              success: {
+                duration: 3000,
+              },
+              error: {
+                duration: 10000,
+              },
+              style: {
+                fontSize: "16px",
+                maxWidth: "500px",
+                padding: "13px 16px",
+                zIndex: 3,
+              },
+            }}
+          />
           {/* Hydrate the client-side cache with server-fetched data */}
           <HydrationBoundary state={dehydratedState}>
             <PopoverManagerProvider>
               <SidebarProvider>
-                <IsOldPageProvider>
-                  <Toaster
-                    position="bottom-center"
-                    gutter={12}
-                    containerStyle={{
-                      margin: "8px",
-                    }}
-                    toastOptions={{
-                      className: clsx(
-                        "!bg-gray-100 !text-gray-800 !font-medium leading-[1.3] !border !border-gray-300 [&>div]:!my-0",
-                        {
-                          "dark:!bg-gray-700 dark:!text-gray-200 !text-base dark:!border-gray-600":
-                            !isRoot,
-                          "": isRoot,
-                        },
-                      ),
-                      success: {
-                        duration: 3000,
-                      },
-                      error: {
-                        duration: 10000,
-                      },
-                      style: {
-                        fontSize: "16px",
-                        maxWidth: "500px",
-                        padding: "13px 16px",
-                        zIndex: 3,
-                      },
-                    }}
-                  />
-                  {children}
-                </IsOldPageProvider>
+                <IsOldPageProvider>{children}</IsOldPageProvider>
               </SidebarProvider>
             </PopoverManagerProvider>
           </HydrationBoundary>
